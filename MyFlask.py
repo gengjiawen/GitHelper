ce71f5541de1af40cd1677f7dcd38a545389fcd6
gengjiawen <technicalcute@gmail.com>
2016-08-04 23:55:38 +0800

    init ui

--- MyFlask.py
+++ MyFlask.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/download', methods=['POST'])
+def download():
+    # http://stackoverflow.com/a/20341272/1713757
+    git_url = request.values.get("git_url")
+    # git_url = request.form["git_url"]
+    print(git_url)
     return 'Hello, World!'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
